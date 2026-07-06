7c9022c7a05e5b6ba3f6779ee12154679d19ba95
huzaifabashir1727 <bashirhuzaifa71.gmail.com>
2026-07-06 05:49:30 +0500

    decorator practice

---
+++ Week3/lymbda.py
@@ -0,0 +1,11 @@
+# persons = [{"name": "Alice", "age": 30}, {"name": "Bob", "age": 25}, {"name": "Charlie", "age": 35}, {"name": "David", "age": 2}]
+
+# persons.sort(key = lambda person : person["age"])
+# print(persons)
+
+nums = [1, 2, 3, 4]
+
+nums.map(lambda n : n * 2)
+
+# list(map(lambda n : n * 2, nums))
+print(nums)
--- Week3/timer.py
+++ Week3/timer.py
@@ -1,2 +1,23 @@
 import time
 
+list = [2,4,6,8,10]
+
+def decorator(task):
+    def timer():
+        Starttime = time.time()
+        Functioncall = task()
+        end = time.time()
+        
+        return Functioncall
+    
+    return timer
+
+@decorator
+def increment():
+    for num in list:
+        num = num + 2
+
+@decorator
+def Poppingmethod():
+    list.pop()
+    
